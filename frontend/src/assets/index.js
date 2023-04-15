5e0399f05a042fd696551cdf3a6a9d7a6f541cf5
Yaqub Mahmoud <yaqub320@gmail.com>
2023-04-15 15:48:31 +0900

    navbar

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -2,10 +2,14 @@ import createProject from './create-project.svg';
 import dashboard from './dashboard.svg';
 import profile from './profile.svg';
 import sun from './sun.svg';
+import search from './search.svg';
+import thirdweb from './thirdweb.png';
 
 export {
   createProject,
   dashboard,
   profile,
   sun,
+  search,
+  thirdweb,
 };
