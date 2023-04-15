bc34de35dcf81355475a71cd0d918d139d97afd8
Yaqub Mahmoud <yaqub320@gmail.com>
2023-04-15 12:04:50 +0900

    changed daisy ui theme

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import "../styles/globals.css"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative sm:-8 p-4 min-h-screen flex flex-row'>
+      <div className='sm:flex hidden mr-10'>
+        Sidebar
+      </div>
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+        Navbar
+      </div>
+      App
+    </div>
   )
 }
 
