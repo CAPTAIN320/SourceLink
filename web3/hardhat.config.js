50b6f67d97c08d6bb1dd63fe8ff580bd4b8997f1
Yaqub Mahmoud <yaqub320@gmail.com>
2023-04-15 10:34:54 +0900

    Update hardhat.config.js

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -2,13 +2,17 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
-    defaultNetwork: 'goerli',
+    defaultNetwork: 'mumbai',
     networks: {
       hardhat: {},
       goerli: {
         url: 'https://rpc.ankr.com/eth_goerli',
         accounts: [`0x${process.env.PRIVATE_KEY}`]
-      }
+      },
+      mumbai: {
+        url: 'https://rpc.ankr.com/polygon_mumbai',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      },
     },
     settings: {
       optimizer: {
