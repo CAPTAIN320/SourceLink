9cfb20c32a64a721f584bee1eb46434e4e1ef068
Yaqub Mahmoud <yaqub320@gmail.com>
2023-04-15 19:00:05 +0900

    Update

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -14,11 +14,11 @@ const Navbar = () => {
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
-      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
-        <input type="text" placeholder="Search for projects" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] rounded-[100px]">
+        <input type="text" placeholder="Search for projects" className="flex w-full font-epilogue font-normal text-[14px] placeholder: outline-none" />
 
-        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
-          <img src={search} alt="search" className="w-[15px] h-[15px] object-contain"/>
+        <div className="w-[72px] h-full rounded-[20px] bg-primary flex justify-center items-center cursor-pointer">
+          <img src={search} alt="search" className="w-[15px] h-[15px] object-contain text-primary-content"/>
         </div>
       </div>
 
@@ -26,23 +26,17 @@ const Navbar = () => {
         <CustomButton
           btnType="button"
           title={address ? 'Create a project' : 'Connect'}
-          styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+          styles={address ? 'bg-primary text-primary-content' : 'bg-secondary text-secondary-content'}
           handleClick={() => {
             if(address) navigate('create-project')
             else connect()
           }}
         />
-
-        <Link to="/profile">
-          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
-            <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
-          </div>
-        </Link>
       </div>
 
       {/* Small screen navigation */}
         <div className="sm:hidden flex justify-between items-center relative">
-        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+        <div className="w-[40px] h-[40px] rounded-[10px] flex justify-center items-center cursor-pointer">
             <img src={logo} alt="user" className="w-[60%] h-[60%] object-contain" />
           </div>
 
@@ -53,12 +47,12 @@ const Navbar = () => {
             onClick={() => setToggleDrawer((prev) => !prev)}
           />
 
-          <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
+          <div className={`absolute top-[60px] right-0 left-0 bg-primary z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
             <ul className="mb-4">
               {navlinks.map((link) => (
                 <li
                   key={link.name}
-                  className={`flex p-4 ${isActive === link.name && 'bg-[#3a3a43]'}`}
+                  className={`flex p-4 ${isActive === link.name && 'bg-primary-content'}`}
                   onClick={() => {
                     setIsActive(link.name);
                     setToggleDrawer(false);
@@ -70,7 +64,7 @@ const Navbar = () => {
                     alt={link.name}
                     className={`w-[24px] h-[24px] object-contain ${isActive === link.name ? 'grayscale-0' : 'grayscale'}`}
                   />
-                  <p className={`ml-[20px] font-epilogue font-semibold text-[14px] ${isActive === link.name ? 'text-[#1dc071]' : 'text-[#808191]'}`}>
+                  <p className={`ml-[20px] font-epilogue font-semibold text-[14px] ${isActive === link.name ? 'text-primary' : 'text-primary-content'}`}>
                     {link.name}
                   </p>
                 </li>
@@ -80,8 +74,8 @@ const Navbar = () => {
             <div className="flex mx-4">
             <CustomButton
               btnType="button"
-              title={address ? 'Create a campaign' : 'Connect'}
-              styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+              title={address ? 'Create a project' : 'Connect'}
+              styles={address ? 'bg-primary text-primary-content' : 'bg-secondary text-secondary-content'}
               handleClick={() => {
                 if(address) navigate('create-project')
                 else connect();
