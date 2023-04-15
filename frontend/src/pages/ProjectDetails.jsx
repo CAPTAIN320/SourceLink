244220751f66651590239c8cbb3f953e5d000310
Yaqub Mahmoud <yaqub320@gmail.com>
2023-04-16 03:08:54 +0900

    Update

--- frontend/src/pages/ProjectDetails.jsx
+++ frontend/src/pages/ProjectDetails.jsx
@@ -60,29 +60,25 @@ const ProjectDetails = () => {
       <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
         <div className="flex-[2] flex flex-col gap-[40px]">
           <div>
-            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Creator</h4>
+            <h4 className="font-epilogue font-semibold text-[18px] uppercase">Creator</h4>
 
             <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
-              <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
-                <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain"/>
-              </div>
               <div>
-                <h4 className="font-epilogue font-semibold text-[14px] text-white break-all">{state.owner}</h4>
-                <p className="mt-[4px] font-epilogue font-normal text-[12px] text-[#808191]">10 Projects</p>
+                <h4 className="font-epilogue font-semibold text-[14px] break-all">{state.owner}</h4>
               </div>
             </div>
           </div>
 
           <div>
-            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Story</h4>
+            <h4 className="font-epilogue font-semibold text-[18px] uppercase">Story</h4>
 
               <div className="mt-[20px]">
-                <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">{state.description}</p>
+                <p className="font-epilogue font-normal text-[16px] leading-[26px] text-justify">{state.description}</p>
               </div>
           </div>
 
           <div>
-            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Supporters</h4>
+            <h4 className="font-epilogue font-semibold text-[18px] uppercase">Supporters</h4>
 
               <div className="mt-[20px] flex flex-col gap-4">
                 {supporters.length > 0 ? supporters.map((item, index) => (
@@ -91,17 +87,17 @@ const ProjectDetails = () => {
                     <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">{item.donation}</p>
                   </div>
                 )) : (
-                  <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">No supporters yet. Be the first one!</p>
+                  <p className="font-epilogue font-normal text-[16px] leading-[26px] text-justify">No supporters yet. Be the first one!</p>
                 )}
               </div>
           </div>
         </div>
 
         <div className="flex-1">
-          <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Fund</h4>
+          <h4 className="font-epilogue font-semibold text-[18px] uppercase">Fund</h4>
 
-          <div className="mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]">
-            <p className="font-epilogue fount-medium text-[20px] leading-[30px] text-center text-[#808191]">
+          <div className="mt-[20px] flex flex-col p-4 bg-secondary rounded-[10px]">
+            <p className="font-epilogue fount-medium text-[20px] leading-[30px] text-center text-secondary-content">
               Fund the project
             </p>
             <div className="mt-[30px]">
@@ -109,20 +105,19 @@ const ProjectDetails = () => {
                 type="number"
                 placeholder="ETH 0.1"
                 step="0.01"
-                className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
+                className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-primary-focus bg-secondary-focus font-epilogue text-secondary-content text-[18px] leading-[30px] placeholder:text-secondary-content rounded-[10px]"
                 value={amount}
                 onChange={(e) => setAmount(e.target.value)}
               />
 
-              <div className="my-[20px] p-4 bg-[#13131a] rounded-[10px]">
-                <h4 className="font-epilogue font-semibold text-[14px] leading-[22px] text-white">Back it because you believe in it.</h4>
-                <p className="mt-[20px] font-epilogue font-normal leading-[22px] text-[#808191]">Support the project for no reward, just because it speaks to you.</p>
+              <div className="my-[20px] p-4 bg-secondary rounded-[10px]">
+                <p className="mt-[20px] font-epilogue font-normal leading-[22px] text-secondary-content">Support the project for no reward, just because it speaks to you.</p>
               </div>
 
               <CustomButton
                 btnType="button"
                 title="Fund Project"
-                styles="w-full bg-[#8c6dfd]"
+                styles="w-full bg-primary"
                 handleClick={handleDonate}
               />
             </div>
