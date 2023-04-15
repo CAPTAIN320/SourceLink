b1c3b58416cc42570f35b176f74dcd191f199969
Yaqub Mahmoud <yaqub320@gmail.com>
2023-04-15 12:13:25 +0900

    Added components

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import "../styles/globals.css"
 
+import Sidebar from '@/components/Sidebar'
+import Navbar from '@/components/Navbar'
+
+import ProjectDetails  from './ProjectDetails'
+import CreateProject from './CreateProject'
+import Profile from './Profile'
+import Home from './Home'
+
 const App = () => {
   return (
     <div className='relative sm:-8 p-4 min-h-screen flex flex-row'>
       <div className='sm:flex hidden mr-10'>
-        Sidebar
+        <Sidebar />
       </div>
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
-        Navbar
+        <Navbar />
+        <Home />
       </div>
       App
     </div>
